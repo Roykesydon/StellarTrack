ce0c38942365010cb1580311fdf0769a45097733
Roykesydon <roy23381547@gmail.com>
2020-11-21 14:18:02 +0800

    add test_scene and RWD

--- Star/index.js
+++ Star/index.js
@@ -1,18 +1,59 @@
-renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight) // 場景大小
-renderer.setClearColor(0xeeeeee, 1.0) // 預設背景顏色
-renderer.shadowMap.enable = true // 陰影效果
+let renderer, scene, camera, container;
+let test_object;
 
-// 將渲染器的 DOM 綁到網頁上
-let container = document.getElementById("threeJS");
-container.appendChild(renderer.domElement);
-// document.body.appendChild(renderer.domElement)
-function init(){
-    scene = new THREE.Scene()
+function create_test_object() {
+    const geometry = new THREE.BoxGeometry(1, 1, 1); // 幾何體
+    const material = new THREE.MeshPhongMaterial({
+        color: 0x0000ff,
+    }); // 材質
+    test_object = new THREE.Mesh(geometry, material);
+    test_object.position.set(0, 0, 0);
+    return test_object;
+}
+function animate(target) {
+    target.rotation.x += 0.01;
+    target.rotation.y += 0.01;
+}
+
+function init() {
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer();
+    container = document.getElementById("threeJS");
+    console.log("container.width= " + container.offsetWidth);
+    console.log("container.height= " + container.offsetHeight);
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
+    renderer.setClearColor(0xccccee, 1.0);
+    renderer.shadowMap.enable = true;
+
+    camera = new THREE.PerspectiveCamera(
+        45, //視角
+        container.offsetWidth / container.offsetHeight, //寬高比
+        0.1, //最近的渲染距離
+        100 //最遠的渲染距離
+    );
+    camera.position.set(10, 10, 10); // 位置
+    camera.lookAt(scene.position); // 看的位置
+
+    let pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(10, 10, -10);
+    scene.add(pointLight);
+
+    scene.add(create_test_object());
+
+    container.appendChild(renderer.domElement);
+}
+window.addEventListener("resize", function () {
+    camera.aspect = container.offsetWidth / container.offsetHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
+});
+function render(){
+    animate(test_object);
+    requestAnimationFrame(render);
+    renderer.render(scene,camera);
 }
 
 
 
-
 init();
-renderer();
+render();
