87eb5b4cf5fbd9f5afd15ca43826c6dd14f4ddac
Roykesydon <roy23381547@gmail.com>
2020-12-12 17:03:15 +0800

    test

--- Star/lib/index.js
+++ Star/lib/index.js
@@ -943,7 +943,7 @@ function create_england_house() {
                     }
                 });
 
-                console.log("bigben16:56");
+                console.log("bigben17:03");
                 scene_house = object;
                 scene.add(scene_house);
             },
