85da96c027bd1de110335e2bafe64ae3833f3e5a
Roykesydon <roy23381547@gmail.com>
2020-12-12 23:55:38 +0800

    test

--- Star/lib/index.js
+++ Star/lib/index.js
@@ -927,9 +927,9 @@ function create_england_house() {
             "bigben.obj",
             function (object) {
                 object.position.y = -17.5;
-                object.position.x=0;
-                object.position.z=20;
-                object.scale.set(0.53, 0.53, 0.53);
+                object.position.x=2;
+                object.position.z=18;
+                object.scale.set(0.5, 0.5, 0.5);
 
                 object.traverse(function (obj) {
                     if (obj instanceof THREE.Mesh) {
