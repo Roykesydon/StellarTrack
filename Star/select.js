bb35492b2080195fb08007bfdcbe134f5bab70e7
Roykesydone <roy23381547@gmail.com>
2021-01-01 08:30:01 +0800

    add bootstrap to button

--- Star/select.js
+++ Star/select.js
@@ -16,7 +16,16 @@ $(document).ready(function(){
                 $("select").append("<option value='"+i+"'>"+field+"</option>");
             });
         });
-    $("#choose1").append("</select>");
+        // $("#choose1").append("  <div class='dropdown'>\
+        // <button type='button' class='btn btn-primary dropdown-toggle' data-toggle='dropdown' id='selected_list'>\
+        //   Dropdown button\
+        // </button><div class='dropdown-menu' name='country' id='country'>");
+        // $.getJSON("https://roykesydon.github.io/StellarTrack/Star/country.json", function(result){
+        //     $.each(result, function(i, field){
+        //         $("#country").append("<a class='dropdown-item' value='"+i+"'  >"+field+"</a>");
+        //     });
+        // });
+    $("#choose1").append("</div></div>");
     flag=1;
     $("#choose1").css("display", "block");
     $("#choose2").css("display", "none");
@@ -58,7 +67,7 @@ function connect(){
         //document.getElementById("latitude").value="";
         city="default";
     }
-    document.getElementById("result").innerHTML="緯度:"+la;
+    document.getElementById("result").innerHTML="<br>緯度:"+la;
     if(choose_season==0) document.getElementById("result").innerHTML+="<br>你現在選的季節是:<font style='color:red'>春秋</font>";
     if(choose_season==1) document.getElementById("result").innerHTML+="<br>你現在選的季節是:<font style='color:red'>夏</font>";
     if(choose_season==2) document.getElementById("result").innerHTML+="<br>你現在選的季節是:<font style='color:red'>冬</font>";
@@ -74,7 +83,7 @@ function getLocation(){
     navigator.geolocation.getCurrentPosition(showPosition);
 }
 function showPosition(position){
-    document.getElementById("result").innerHTML="當前座標:緯度: " + position.coords.latitude.toFixed(1) + 
+    document.getElementById("result").innerHTML="<br>當前座標:緯度: " + position.coords.latitude.toFixed(1) + 
     "<br />經度: " + position.coords.longitude.toFixed(1)+"<br> 按下Present以顯示當前位置太陽軌跡圖";
     la=position.coords.latitude.toFixed(1);
     flag=2;
